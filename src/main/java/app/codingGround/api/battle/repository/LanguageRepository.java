ed84d1c4159c148a48a1cce0d43ac5574e86b248
realomadia <hhun6840@naver.com>
2023-11-29 20:26:33 +0900

    [:sparkles: DEV] 바디렝귀지

--- src/main/java/app/codingGround/api/battle/repository/LanguageRepository.java
+++ src/main/java/app/codingGround/api/battle/repository/LanguageRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+// LanguageRepository
 @Repository
 public interface LanguageRepository extends JpaRepository<Language, Long> {
 
