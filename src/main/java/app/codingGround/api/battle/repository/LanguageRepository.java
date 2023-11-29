97ddad7a8abc4ad01afc5c9c6e3894d3863c318b
realomadia <hhun6840@naver.com>
2023-11-29 20:26:52 +0900

    [:sparkles: DEV] 바디렝귀지 해결

--- src/main/java/app/codingGround/api/battle/repository/LanguageRepository.java
+++ src/main/java/app/codingGround/api/battle/repository/LanguageRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// LanguageRepository
 @Repository
 public interface LanguageRepository extends JpaRepository<Language, Long> {
 
