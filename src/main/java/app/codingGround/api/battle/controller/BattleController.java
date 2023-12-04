7fef35ca5f48299e6253e6c951dfb8dd6d493cb2
realomadia <hhun6840@naver.com>
2023-12-04 21:06:29 +0900

    [:fire: FIX] LUCY 제거 VUE.JS를 사용해서 굳이 사용 안해도 될것같음, stompClient VUEX로 관리 테스트

--- src/main/java/app/codingGround/api/battle/controller/BattleController.java
+++ src/main/java/app/codingGround/api/battle/controller/BattleController.java
@@ -16,6 +16,7 @@ import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -43,19 +44,27 @@ public class BattleController {
         }
 //      게임 타입이 WAIT 이고, 유저 인원수가 8명일때! 게임시작 전송
         String gameStatus = battleService.getGameStatus(gameId);
-        if(gameStatus.equals("WAIT") && userCount == 2){
+        if(gameStatus.equals("WAIT") && userCount == 2){ // 테스트를 위해 2로 해놓음
             battleService.startGame(gameId);
+            LocalDateTime currentTime = LocalDateTime.now();
+            LocalDateTime futureTime = currentTime.plusSeconds(10);
 
 
-            messagingTemplate.convertAndSend("/topic/public/gameStart/"+gameId, true);
+            messagingTemplate.convertAndSend("/topic/public/gameStart/"+gameId, futureTime);
         }
+    }
+    @MessageMapping("/send")
+    public void timer() {
+        // Add username in web socket session
+        String gameId = "dwadwadwa";
+        System.out.println("herererererere");
+        messagingTemplate.convertAndSend("/topic/public/gameStart/"+gameId, gameId);
     }
 
     @MessageMapping("/chat.addUser")
     @SendTo("/topic/public")
     public ChatMessage addUser(@Payload ChatMessage chatMessage) {
         // Add username in web socket session
-
 
 
         return chatMessage;
--- src/main/java/app/codingGround/global/config/web/WebConfig.java
+++ src/main/java/app/codingGround/global/config/web/WebConfig.java
@@ -1,46 +1,53 @@
-package app.codingGround.global.config.web;
 
-import app.codingGround.global.config.xss.HtmlCharacterEscapes;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// vue.js 를 사용하기때문에 lucy 사용하지않음
 
-import java.util.Arrays;
-import java.util.List;
 
-@Configuration
-@RequiredArgsConstructor
-public class WebConfig implements WebMvcConfigurer {
 
-    private final ObjectMapper objectMapper;
-
-    @Bean
-    public FilterRegistrationBean<XssEscapeServletFilter> filterRegistrationBean() {
-        FilterRegistrationBean<XssEscapeServletFilter> filterRegistration = new FilterRegistrationBean<>();
-        filterRegistration.setFilter(new XssEscapeServletFilter());
-        filterRegistration.setOrder(1);
-        filterRegistration.addUrlPatterns("/*");
-        filterRegistration.setUrlPatterns(Arrays.asList("/mypage/record/*"));
-
-        return filterRegistration;
-    }
-
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(jsonEscapeConverter());
-    }
-
-    @Bean
-    public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
-        ObjectMapper copy = objectMapper.copy();
-        copy.getFactory().setCharacterEscapes(new HtmlCharacterEscapes());
-        return new MappingJackson2HttpMessageConverter(copy);
-    }
-
-}
+//package app.codingGround.global.config.web;
+//
+//import app.codingGround.global.config.xss.HtmlCharacterEscapes;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.navercorp.lucy.security.xss.servletfilter.XssEscapeServletFilter;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.converter.HttpMessageConverter;
+//import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+//
+//import java.util.Collections;
+//import java.util.List;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    private final ObjectMapper objectMapper;
+//
+//    @Bean
+//    public FilterRegistrationBean<XssEscapeServletFilter> filterRegistrationBean() {
+//        FilterRegistrationBean<XssEscapeServletFilter> filterRegistration = new FilterRegistrationBean<>();
+//        XssEscapeServletFilter xssEscapeServletFilter = new XssEscapeServletFilter();
+//
+//        // Set up URL patterns to include
+//        filterRegistration.setFilter(xssEscapeServletFilter);
+//        filterRegistration.setOrder(1);
+//        filterRegistration.addUrlPatterns("/*");
+//
+//        // Exclude /api/mypage/gamerecord/* from filter
+//
+//        return filterRegistration;
+//    }
+//    @Override
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//         converters.add(jsonEscapeConverter());
+//    }
+//    @Bean
+//    public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
+//        ObjectMapper copy = objectMapper.copy();
+//        copy.getFactory().setCharacterEscapes(new HtmlCharacterEscapes());
+//        return new MappingJackson2HttpMessageConverter();
+//    }
+//}
