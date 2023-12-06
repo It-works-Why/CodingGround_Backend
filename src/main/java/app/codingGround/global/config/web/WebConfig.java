47a3e34a67e40cee26c9ccc87ebe04c41380c547
Prkhgwan <crer03@gmail.com>
2023-12-06 14:03:03 +0900

    :construction: WebConfig.java 수정

--- src/main/java/app/codingGround/global/config/web/WebConfig.java
+++ src/main/java/app/codingGround/global/config/web/WebConfig.java
@@ -14,8 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS를 허용하도록 설정
-                .allowedOrigins("*") // 모든 출처(Origin)에서 접근을 허용합니다. 실제 제품에서는 필요에 따라 특정 origin으로 제한해야 합니다.
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메소드 설정
+                .allowedOriginPatterns("http://bsdev16-codingground-frontend.s3-website.ca-central-1.amazonaws.com") // Vue.js 애플리케이션의 도메인을 여기에 패턴으로 추가
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드 설정
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 인증 정보 (쿠키, 인증 헤더 등) 전달 허용
                 .maxAge(3600); // CORS preflight 요청 결과를 캐시하는 시간 설정
