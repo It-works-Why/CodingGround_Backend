3ed068ad76be4be10a6eacb42d295b940a7230fc
realomadia <hhun6840@naver.com>
2023-12-06 16:01:47 +0900

    [:fire: FIX] WEBCONFIG 수정

--- src/main/java/app/codingGround/global/config/web/WebConfig.java
+++ src/main/java/app/codingGround/global/config/web/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://bsdev16-codingground-frontend.s3-website.ca-central-1.amazonaws.com",
                         "http://localhost:8080"
                 )
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드 설정
+                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH", "get","post","put","delete","patch") // 허용할 HTTP 메소드 설정
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 인증 정보 (쿠키, 인증 헤더 등) 전달 허용
                 .maxAge(3600); // CORS preflight 요청 결과를 캐시하는 시간 설정
