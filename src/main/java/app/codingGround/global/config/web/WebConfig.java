2b700e60311b84a3b95506c7083501c027d2d71c
Prkhgwan <crer03@gmail.com>
2023-12-10 14:20:12 +0900

    :white_check_mark: DB, Reids 변경, WebConfig.java의 불필요한 OriginPatterns 삭제

--- src/main/java/app/codingGround/global/config/web/WebConfig.java
+++ src/main/java/app/codingGround/global/config/web/WebConfig.java
@@ -15,43 +15,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS를 허용하도록 설정
                 .allowedOriginPatterns(
-                        "http://bsdev16-codingground-frontend.s3-website.ca-central-1.amazonaws.com",
                         "http://api.mzc-codingground.click",
-                        "https://api.mzc-codingground.click",
-                        "http://www.mzc-codingground.click",
-                        "https://www.mzc-codingground.click"
+                        "https://api.mzc-codingground.click"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH", "get","post","put","delete","patch") // 허용할 HTTP 메소드 설정
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 인증 정보 (쿠키, 인증 헤더 등) 전달 허용
                 .maxAge(3600); // CORS preflight 요청 결과를 캐시하는 시간 설정
     }
-
-
-//    private final ObjectMapper objectMapper;
-
-//    @Bean
-//    public FilterRegistrationBean<XssEscapeServletFilter> filterRegistrationBean() {
-//        FilterRegistrationBean<XssEscapeServletFilter> filterRegistration = new FilterRegistrationBean<>();
-//        XssEscapeServletFilter xssEscapeServletFilter = new XssEscapeServletFilter();
-//
-//        // Set up URL patterns to include
-//        filterRegistration.setFilter(xssEscapeServletFilter);
-//        filterRegistration.setOrder(1);
-//        filterRegistration.addUrlPatterns("/*");
-//
-//        // Exclude /api/mypage/gamerecord/* from filter
-//
-//        return filterRegistration;
-//    }
-//    @Override
-//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-//         converters.add(jsonEscapeConverter());
-//    }
-//    @Bean
-//    public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
-//        ObjectMapper copy = objectMapper.copy();
-//        copy.getFactory().setCharacterEscapes(new HtmlCharacterEscapes());
-//        return new MappingJackson2HttpMessageConverter();
-//    }
 }
