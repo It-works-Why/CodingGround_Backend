901eb35449e8593c0291603cc75cd42e5b7bc102
Prkhgwan <crer03@gmail.com>
2023-12-12 09:25:17 +0900

    :bug: Judge0 API 요청주소 수정

--- src/main/java/app/codingGround/global/utils/Judge0Util.java
+++ src/main/java/app/codingGround/global/utils/Judge0Util.java
@@ -17,7 +17,7 @@ public class Judge0Util {
     public static List<String> runCode(CodeData codeData, List<TestCaseDto> testCaseDtos) {
 
         try {
-            String url = "http://bsdev16-jugd0-lb-1b4457839ee804dc.elb.ca-central-1.amazonaws.com:2358";
+            String url = "http://bsdev16-jugd0-lb-1b4457839ee804dc.elb.ca-central-1.amazonaws.com:2358/submissions/";
             OkHttpClient client = new OkHttpClient();
             Response response = null;
             List<String> tokens = new ArrayList<>();
