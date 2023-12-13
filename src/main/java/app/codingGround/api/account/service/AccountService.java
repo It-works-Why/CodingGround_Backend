972ab3f4bed4051ac4d3144939f890f3646a9bdf
shshe22 <shshe22@naver.com>
2023-12-13 16:58:41 +0900

    :rotating_light: fix : 시즌 번호 저장 방식 변경

--- src/main/java/app/codingGround/api/account/service/AccountService.java
+++ src/main/java/app/codingGround/api/account/service/AccountService.java
@@ -98,30 +98,9 @@ public class AccountService {
         user.setUserProfileImg(userRegisterDto.getUserProfileImg());
         accountRepository.save(user);
 
-        // user season 테이블 데이터 생성
-        LocalDateTime nowDate = LocalDateTime.now();
-
-        DateTimeFormatter dateFormatYear =  DateTimeFormatter.ofPattern("yyyy");
-        String year = dateFormatYear.format(nowDate);
-
-        DateTimeFormatter dateFormatMonth = DateTimeFormatter.ofPattern("MM");
-        String month = dateFormatMonth.format(nowDate);
-
-        String seasonName = null;
-
-        if (month.equals("1") || month.equals("2") || month.equals("3")) {
-            seasonName = year + "-1";
-        } else if (month.equals("4") || month.equals("5") || month.equals("6")) {
-            seasonName = year + "-2";
-        } else if (month.equals("7") || month.equals("8") || month.equals("9")) {
-            seasonName = year + "-3";
-        } else {
-            seasonName = year + "-4";
-        }
-
         User user2 = accountRepository.findByUserEmail(userRegisterDto.getUserEmail());
         Rank rank = rankingRepository.findByRankNum(5L);
-        Season season = seasonRepository.findBySeasonName(seasonName);
+        Season season = seasonRepository.findFirstByOrderBySeasonNumDesc();
 
         UserSeason userSeason = new UserSeason();
         userSeason.setUser(user2);
