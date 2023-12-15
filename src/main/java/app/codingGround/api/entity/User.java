d3259e185b50878bbe47dd063c1575fc04443fee
Prkhgwan <crer03@gmail.com>
2023-12-15 12:11:33 +0900

    :white_check_mark: S3 Bucket 리전 변경으로 인한 매핑 변경

--- src/main/java/app/codingGround/api/entity/User.java
+++ src/main/java/app/codingGround/api/entity/User.java
@@ -93,7 +93,7 @@ public class User implements UserDetails {
         userRegDate = new Timestamp(new Date().getTime());
         userStatus = "ACTIVE";
         userRole = "USER";
-        userProfileImg = "https://bsdev16-codingground.s3.ca-central-1.amazonaws.com/asset/DefaultProfile.png";
+        userProfileImg = "https://bsdev16-img-bucket.s3.ap-east-1.amazonaws.com/asset/DefaultProfile.png";
     }
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
